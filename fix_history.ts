8c600519d2cb3416ae96a21848826556fef628f8
ammhskyinnohub-byte <ammh.skyinnohub@gmail.com>
2026-05-25 01:33:54 +0630

    delete

--- fix_history.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-const filePath = 'src/components/HistoryList.tsx';
-let content = fs.readFileSync(filePath, 'utf8');
-
-content = content.replace(/bg-white dark:bg-white dark:bg-white\/\[0\.03\]/g, 'bg-white dark:bg-white/[0.03]');
-content = content.replace(/hover:text-black dark:text-white/g, 'dark:hover:text-white hover:text-black');
-
-fs.writeFileSync(filePath, content);
